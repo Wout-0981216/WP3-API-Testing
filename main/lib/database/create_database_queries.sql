36f9f7f33b07d042649e4c252c596082e4d8ea97
Wout-0981216 <144107290+Wout-0981216@users.noreply.github.com>
2024-02-13 17:33:21 +0100

    changed database and added a init_db file

--- main/lib/database/create_database_queries.sql
+++ main/lib/database/create_database_queries.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Organisatie (
+CREATE TABLE IF NOT EXISTS Organisatie (
   "id" integer PRIMARY KEY,
   "aam" varchar,
   "type" varchar,
@@ -9,12 +9,20 @@ CREATE TABLE Organisatie (
   "telefoonnummer" integer,
   "overige_details" varchar
 );
-CREATE TABLE Beperking (
+CREATE TABLE IF NOT EXISTS Beperking (
   "id" integer PRIMARY KEY,
   "beperking" varchar,
   "type_beperking" varchar
 );
-CREATE TABLE Onderzoek (
+CREATE TABLE IF NOT EXISTS Beheerder (
+  "id" integer PRIMARY KEY,
+  "gebuikersnaam" varchar UNIQUE,
+  "wachtwoord" varchar,
+  "voornaam" varchar,
+  "achternaam" varchar,
+  "team" varchar
+);
+CREATE TABLE IF NOT EXISTS Onderzoek (
   "id" integer PRIMARY KEY,
   "titel" varchar,
   "beschikbaar" integer,
@@ -29,10 +37,14 @@ CREATE TABLE Onderzoek (
   "doelgroep_leeftijd_tot" integer,
   "doelgroep_beperking" integer,
   "organisatie_id" integer,
+  "status" varchar,
+  "datum_status_update" timestamp,
+  "beheerder_id" integer,
+  FOREIGN KEY ("beheerder_id") REFERENCES Beheerder("id")
   FOREIGN KEY ("doelgroep_beperking") REFERENCES Beperking("id")
   FOREIGN KEY ("organisatie_id") REFERENCES Organisatie("id")
 );
-CREATE TABLE IF NOT EXISTS "Ervaringsdeskundige" (
+CREATE TABLE IF NOT EXISTS Ervaringsdeskundige (
   "id" integer PRIMARY KEY,
   "voornaam" varchar,
   "achternaam" varchar,
@@ -52,52 +64,33 @@ CREATE TABLE IF NOT EXISTS "Ervaringsdeskundige" (
   "voorkeur_benadering" varchar,
   "type_onderzoek" varchar,
   "bijzonderheden_beschikbaarheid" varchar
+  "status" varchar,
+  "beheerder_id" integer,
+  "datum_status_update" timestamp,
+  FOREIGN KEY ("beheerder_id") REFERENCES Beheerder("id")
 );
-CREATE TABLE IF NOT EXISTS "Beheerder" (
-  "gebuikersnaam" varchar PRIMARY KEY,
-  "wachtwoord" varchar,
-  "voornaam" varchar,
-  "achternaam" varchar,
-  "team" varchar
-);
-CREATE TABLE IF NOT EXISTS "Onderzoeks_vraag" (
+CREATE TABLE IF NOT EXISTS Onderzoeks_vraag (
   "id" integer PRIMARY KEY,
   "titel" varchar,
   "vraag" text,
   "onderzoek_id" integer,
   FOREIGN KEY("onderzoek_id") REFERENCES Onderzoek("id")
 );
-CREATE TABLE IF NOT EXISTS "Ervaringsdeskundige_beperking" (
+CREATE TABLE IF NOT EXISTS Ervaringsdeskundige_beperking (
   "ervaringsdeskundige_id" integer,
   "beperking_id" integer,
   PRIMARY KEY ("ervaringsdeskundige_id", "beperking_id")
   FOREIGN KEY ("ervaringsdeskundige_id") REFERENCES Ervaringsdeskundige("id"),
   FOREIGN KEY ("beperking_id") REFERENCES Beperking("id")
 );
-CREATE TABLE IF NOT EXISTS "Status_onderzoek" (
-  "onderzoek_id" integer PRIMARY KEY,
-  "status" varchar,
-  "beheerder_gebruikersnaam" varchar,
-  "Datum_laatste_status_update" timestamp,
-  FOREIGN KEY ("onderzoek_id") REFERENCES Onderzoek("id"),
-  FOREIGN KEY ("beheerder_gebruikersnaam") REFERENCES Beheerder("gebruikersnaam")
-);
-CREATE TABLE IF NOT EXISTS "Inschrijving_ervaringsdeskundige_onderzoek" (
+CREATE TABLE IF NOT EXISTS Inschrijving_ervaringsdeskundige_onderzoek (
   "ervaringsdeskundige_id" integer,
   "onderzoek_id" integer,
   "status" varchar,
-  "beheerder_gebruikersnaam" integer,
+  "beheerder_id" integer,
   "Datum_laatste_status_update" timestamp,
   PRIMARY KEY ("ervaringsdeskundige_id", "onderzoek_id"),
   FOREIGN KEY ("ervaringsdeskundige_id") REFERENCES Ervaringsdeskundige("id"),
-  FOREIGN KEY ("beheerder_gebruikersnaam") REFERENCES Beheerder("gebruikersnaam"),
+  FOREIGN KEY ("beheerder_id") REFERENCES Beheerder("id"),
   FOREIGN KEY ("onderzoek_id") REFERENCES Onderzoek("id")
-);
-CREATE TABLE IF NOT EXISTS "Status_registratie_ervaringsdeskundige" (
-  "ervaringsdeskundige_id" integer PRIMARY KEY,
-  "status" varchar,
-  "beheerder_gebruikersnaam" integer,
-  "Datum_laatste_status_update" timestamp,
-  FOREIGN KEY ("ervaringsdeskundige_id") REFERENCES Ervaringsdeskundige("id"),
-  FOREIGN KEY ("beheerder_gebruikersnaam") REFERENCES Beheerder("id")
 );
