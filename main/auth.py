5165f35472280e0799cf4d429d3a9b8119b25763
Wout-0981216 <144107290+Wout-0981216@users.noreply.github.com>
2024-02-13 23:11:04 +0100

    added connection from the from to database query

--- main/auth.py
+++ main/auth.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, session
+from flask import Blueprint, render_template, redirect, url_for, flash, session, request
+from database_functions import insert_ervaringsdeskundige_into_database
 
 
 auth_blueprint = Blueprint('auth', __name__)
@@ -15,6 +16,12 @@ def logout():
     flash("U bent succesvol uitgelogd")
     return redirect(url_for("auth.index"))
 
-@auth_blueprint.route("/registration", methods=['GET','POST'])
+@auth_blueprint.route("/registration", methods=['GET'])
 def registration():
+    return render_template('register.html')
+
+@auth_blueprint.route("/registration", methods=['POST'])
+def add_registration():
+    msg = insert_ervaringsdeskundige_into_database(request.form)
+    flash(msg)
     return render_template('register.html')
