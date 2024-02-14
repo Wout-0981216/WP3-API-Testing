d6f3b780816bbacd8fe79577edb99e36573f23f9
Wout-0981216 <144107290+Wout-0981216@users.noreply.github.com>
2024-02-14 14:19:50 +0100

    added comments to explain the code

--- main/static/javascript/javascript.js
+++ main/static/javascript/javascript.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $('#birthdate').attr("max",get_current_date).attr("value",get_current_date);
   $('#birthdate').on("blur",function(){
-    if(under_18()){
+    if(under_18()){ //when you enter a birthdate this blocks the no option if you are under 18
       console.log(under_18())
       $($('#yes').prop('checked', true));
       allow_guardian_inputs();
@@ -11,7 +11,7 @@ $(document).ready(function () {
     }
   });
   $("input[name=guardian]").change(function(){
-    if(under_18()){
+    if(under_18()){ //when you click on yes or no, blocks no if you are under 18
       $($('#yes').prop('checked', true));
       allow_guardian_inputs();
     }
@@ -20,35 +20,32 @@ $(document).ready(function () {
     }
   });
   $('#no').on("click", function(){
-    if(under_18()){
+    if(under_18()){ //can't click on no if you are under 18
       alert("Je bent onder de 18 en moet een voogd of toezichthouder toevoegen!")
     }
   });
 });
 function allow_guardian_inputs(){
-  if($("#yes").is(':checked')){
+  if($("#yes").is(':checked')){ //if yes button is checked, show the inputs for guardian
     $("#guardian_inputs").show();
   }
   else{
-    $("#guardian_inputs").hide();
+    $("#guardian_inputs").hide(); // don't show guardian inputs
   }
 };
 function under_18(){
   var date_18 = new Date(get_current_date());
-  //gets the date before which you need to be born to be 18 or older
-  date_18.setFullYear(date_18.getFullYear()-18);
-  entered_date = new Date($('#birthdate').val())
+  date_18.setFullYear(date_18.getFullYear()-18); //gets the date before which you need to be born to be 18 or older
+  entered_date = new Date($('#birthdate').val()) //the date entered on the form
   if (date_18 < entered_date){
     return true;
-    $($('#yes').prop('checked', true));
-    allow_guardian_inputs();
   }
   else{
     return false;
-    allow_guardian_inputs();
   }
 };
 function get_current_date(){
+  //returns the current date
   var today = new Date();
   var dd = today.getDate();
   var mm = today.getMonth() + 1; //January is 0
