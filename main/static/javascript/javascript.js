1d5d01b59bb3231c38413e89dd3b360145e9d4d7
Wout-0981216 <144107290+Wout-0981216@users.noreply.github.com>
2024-02-14 12:10:13 +0100

    javascript to ensure date entered is not past today

--- main/static/javascript/javascript.js
+++ main/static/javascript/javascript.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  $('#birthdate').attr("max",get_current_date)
   $("input[name=guardian]").change(function(){
     if($("#yes").is(':checked')){
       $("#guardian_inputs").show();
@@ -8,3 +9,22 @@ $(document).ready(function () {
     }
   });
 });
+
+function get_current_date(){
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1; //January is 0
+  var yyyy = today.getFullYear();
+
+  //add '0' to get correct format for day and month
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+
+  if (mm < 10) {
+    mm = '0' + mm;
+  } 
+
+  today = yyyy + '-' + mm + '-' + dd;
+  return today
+}
