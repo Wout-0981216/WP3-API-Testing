c9a737a5b8eb3d0388fca5db419ba6a0aac1c714
Wout-0981216 <144107290+Wout-0981216@users.noreply.github.com>
2024-03-06 14:03:46 +0100

    merge fixes

--- main/auth.py
+++ main/auth.py
@@ -88,23 +88,3 @@ def login_evd():
             return redirect(url_for('auth.login_beheerder'))
 
     return render_template('login_evd.html')
-
-@auth_blueprint.route("/ervaringsdeskundige_overzicht", methods=['GET', 'POST'])
-def evd_overzicht():
-    all_evd = get_all_evd_from_database()
-    return render_template('ervaringsdeskundige_overzicht.html', all_evd=all_evd)
-
-@auth_blueprint.route("/ervaringsdeskundige_goedkeuren/<evd_id>", methods=['GET', 'POST'])
-def confirm_evd(evd_id):
-    confirm_evd_status(evd_id)
-    return redirect(url_for('auth.evd_overzicht'))
-
-@auth_blueprint.route("/ervaringsdeskundige_afkeuren/<evd_id>", methods=['GET', 'POST'])
-def deny_evd(evd_id):
-    deny_evd_status(evd_id)
-    return redirect(url_for('auth.evd_overzicht'))
-
-@auth_blueprint.route("/ervaringsdeskundige_view/<evd_id>", methods=['GET', 'POST'])
-def view_evd(evd_id):
-    evd_info = get_evd_from_database_by_id(evd_id)
-    return render_template('view_ervaringsdeskundige.html', evd_info = evd_info)
--- main/beheerder.py
+++ main/beheerder.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, jsonify
-from database_functions import get_aanvragen, get_onderzoeken, get_evd
+from flask import Blueprint, render_template, redirect, url_for, jsonify
+from database_functions import *
 
 
 beheerder_blueprint = Blueprint('beheerder', __name__)
@@ -23,3 +23,23 @@ def get_onderzoeken_route():
 def get_evder_route():
     evd = get_evd()
     return jsonify({'evd': evd})
+
+@beheerder_blueprint.route("/ervaringsdeskundige_overzicht", methods=['GET', 'POST'])
+def evd_overzicht():
+    all_evd = get_all_evd_from_database()
+    return render_template('ervaringsdeskundige_overzicht.html', all_evd=all_evd)
+
+@beheerder_blueprint.route("/ervaringsdeskundige_goedkeuren/<evd_id>", methods=['GET', 'POST'])
+def confirm_evd(evd_id):
+    confirm_evd_status(evd_id)
+    return redirect(url_for('beheerder.evd_overzicht'))
+
+@beheerder_blueprint.route("/ervaringsdeskundige_afkeuren/<evd_id>", methods=['GET', 'POST'])
+def deny_evd(evd_id):
+    deny_evd_status(evd_id)
+    return redirect(url_for('beheerder.evd_overzicht'))
+
+@beheerder_blueprint.route("/ervaringsdeskundige_view/<evd_id>", methods=['GET', 'POST'])
+def view_evd(evd_id):
+    evd_info = get_evd_from_database_by_id(evd_id)
+    return render_template('view_ervaringsdeskundige.html', evd_info = evd_info)
