01a803cfc2ed6cb467bf0d103c3e9a94204b9c8b
Tjores-1075226 <1075226@hr.nl>
2024-03-13 18:45:56 +0100

    added go back buttons

--- main/app.py
+++ main/app.py
@@ -5,21 +5,12 @@ from ervaringsdeskundige import ervaringsdeskundige_blueprint
 from beheerder import beheerder_blueprint
 
 
-
-
-
 app = Flask(__name__)
 
 app.secret_key = 'super secret key'
 app.register_blueprint(auth_blueprint)
-
 app.register_blueprint(ervaringsdeskundige_blueprint)
-
-
-
 app.register_blueprint(beheerder_blueprint)
-
-
 
 
 if __name__ == '__main__':
